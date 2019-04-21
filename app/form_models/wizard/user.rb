2f2ea6c9b8366b0d00002ea9aa76bdb92bdef892
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-04-21 05:57:37 +0530

    title: Made UI changes
    desc:
    1. Added Strike through design for completed steps
    2. Added animated images
    3. Aligned forms along with images

--- app/form_models/wizard/user.rb
+++ app/form_models/wizard/user.rb
@@ -15,6 +15,13 @@ module Wizard
 
     class Step1 < Base
       validates :email, presence: true, format: { with: /@/ }
+      validates :phone_number, presence: true
+      validates :first_name, presence: true
+      validates :last_name, presence: true
+      validates :address_1, presence: true
+      validates :zip_code, presence: true
+      validates :city, presence: true
+      validates :country, presence: true
     end
 
     class Step2 < Step1
