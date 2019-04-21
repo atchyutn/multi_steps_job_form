60a8fd69623eeb89c79e9c86f622055ef6d5e0ee
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-04-21 12:58:46 +0530

    title: Added new fields
    desc:
    1. Added all the fields of other steps in the application form
    2. wrote a migration for those fields

--- app/form_models/wizard/user.rb
+++ app/form_models/wizard/user.rb
@@ -25,19 +25,36 @@ module Wizard
     end
 
     class Step2 < Step1
-      validates :first_name, presence: true
-      validates :last_name, presence: true
+      validates :university, presence: true
+      validates :highest_qualification, presence: true
+      validates :grade_points_grad, presence: true
+      validates :school, presence: true
+      validates :major, presence: true
+      validates :grade_points_school, presence: true
+      validates :activities, presence: true
+      validates :goal, presence: true
+      validates :opportunities, presence: true
     end
 
     class Step3 < Step2
-      validates :address_1, presence: true
-      validates :zip_code, presence: true
-      validates :city, presence: true
-      validates :country, presence: true
+      validates :employer, presence: true
+      validates :total_exp, presence: true
+      validates :relevant_exp, presence: true
+      validates :cctc, presence: true
+      validates :ectc, presence: true
+      validates :notice_period, presence: true
+      validates :prefered_work_location, presence: true
+      validates :reason_for_change, presence: true
     end
 
     class Step4 < Step3
-      validates :phone_number, presence: true
+      validates :github, presence: true
+      validates :linkedin, presence: true
+      validates :stackoverflow, presence: true
+      validates :blog, presence: true
+      validates :twitter, presence: true
+      validates :skype, presence: true
     end
   end
 end
+
